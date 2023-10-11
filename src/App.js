364ec630e0f2478d5da139b3695ac10b409e5303
bvenceslas <bvenceslas@gmail.com>
2023-10-11 18:11:23 +0200

    refactor steps button and message

--- src/App.js
+++ src/App.js
@@ -30,21 +30,24 @@ function App() {
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
 
-          <p className="message">{`Step ${step} - ${messages[step - 1]}`}</p>
+          {/* <p className="message">{`Step ${step} - ${messages[step - 1]}`}</p> */}
+          <StepMessage step={step}>
+            {messages[step - 1]}
+            {/* <div className="buttons">
+              <Button bgColor="#fff" onClick={() => alert("Well Done!")}>
+                Test me
+              </Button>
+            </div> */}
+          </StepMessage>
 
           <div className="buttons">
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handlePrevious}
-            >
-              Previous
-            </button>
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handleNext}
-            >
-              Next
-            </button>
+            <Button textColor="#fff" bgColor="#7950f2" onClick={handlePrevious}>
+              <span>👈🏽</span> Previous
+            </Button>
+
+            <Button textColor="#fff" bgColor="#7950f2" onClick={handleNext}>
+              Next <span>👈🏽</span>
+            </Button>
           </div>
         </div>
       )}
@@ -53,3 +56,26 @@ function App() {
 }
 
 export default App;
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>STEP {step}</h3>
+      <p>{children}</p>
+    </div>
+  );
+}
+
+function Button({ textColor, bgColor, onClick, children }) {
+  return (
+    <button
+      style={{
+        backgroundColor: bgColor,
+        color: textColor,
+      }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
